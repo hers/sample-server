9fe7f2d2aea16c172a746f269bc3d8ce4455c792
hers <alany2010@live.com>
2024-10-28 14:53:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Getting Started with sample-server
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `npm start`
+
+Runs the server in development mode.
---
+++ data.js
@@ -0,0 +1,19 @@
+module.exports = {
+  products: [
+    {
+      id: 1,
+      name: 'apple',
+      price: 13.78
+    },
+    {
+      id: 2,
+      name: 'orange',
+      price: 5.66
+    },
+    {
+      id: 3,
+      name: 'pear',
+      price: 2.31
+    },
+  ]
+}
